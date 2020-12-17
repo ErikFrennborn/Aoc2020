e61d90be5d29be8dbcc244b9bef67dfc8cf14a0b
Erik Frennborn <erikfre@student.chalmers.se>
2020-12-17 18:54:58 +0100

    Day 13, Task 2 after relearning chinese reminder theorem and way to much weird bugs

--- 13/13.cpp
+++ 13/13.cpp
@@ -31,52 +31,58 @@ int task_1(vector<tuple<int,int>>* bus_ids, int target_time)
     return min_id * min;
 }
 
-// long task_2(vector<tuple<int,int>>* bus_ids)
-// {
-//     long t = 10000000000000;
-//     t -= t%get<0>(bus_ids->at(0));
-//     cout << t%get<0>(bus_ids->at(0)) << endl;
-//     // #pragma omp parallel for schedule(dynamic, 256)
-//     for (;;t += get<0>(bus_ids->at(0)))
-//     {
-//         bool valid = true;
-//         for (int i = 1; i < bus_ids->size(); i++)
-//         {
-//             if ((t+get<1>(bus_ids->at(i)))%get<0>(bus_ids->at(i)) != 0 ) valid = false;
-//         }
-//         if (valid) break;
-//     }
-//     return t;
-// }
 
+// Borrowed modulo inverser, could be ask to reimplement
+long modInverse(long a, long m) 
+{ 
+    long m0 = m; 
+    long y = 0, x = 1; 
+  
+    if (m == 1) 
+        return 0; 
+  
+    while (a > 1) { 
+        // q is quotient 
+        long q = a / m; 
+        long t = m; 
+  
+        // m is remainder now, process same as 
+        // Euclid's algo 
+        m = a % m, a = t; 
+        t = y; 
+  
+        // Update y and x 
+        y = x - q * y; 
+        x = t; 
+    } 
+  
+    // Make x positive 
+    if (x < 0) 
+        x += m0; 
+  
+    return x; 
+} 
 
-long task_2()
+long task_2(vector<tuple<int,int>>* bus_ids)
 {
-/*
-  problem:
-  t % 17 = 0
-  t % 41 = 7
-  t % 523 = 17
-  t % 13 = 35
-  t % 19 = 36
-  t % 23 = 40
-  t % 787 = 48
-  t % 37 = 54
-  
-  eq:
-  t = 17*n1
-  t = 41*n2 +7
-  t = 523*n3 + 17
-  t = 13*n4 + 35
-  t = 19*n5 + 36
-  t = 23*n6 + 40
-  t = 787*n7 + 48
-  t = 37*n8 + 54
- 
-  
+    //Generting N_i 
+    long n = 1;
+    for(int i = 0; i < bus_ids->size();i++)
+    {
+        n *= get<0>(bus_ids->at(i));
+    }
+    long sum = 0;
+    for(int i = 0; i < bus_ids->size();i++)
+    {
+        long n_i = n/get<0>(bus_ids->at(i));
 
-*/
-
+        long b_i = get<1>(bus_ids->at(i));
+        long x_i = modInverse( n_i, get<0>(bus_ids->at(i)));
+        sum += n_i*b_i*x_i;
+    }
+    if (sum < 0) sum += n;
+    
+    return sum % n;
 }
 
 int main()
@@ -85,21 +91,18 @@ int main()
     parse(&data);
     int target_time = stoi(data.at(0));
     vector<tuple<int,int>> bus_ids;
+
     int pos;
     std::string token;  
     int count = 0;
     while ((pos = data.at(1).find(",")) != std::string::npos)
     {
         token = data.at(1).substr(0, pos);
-        if (token.at(0) !='x') bus_ids.push_back(tuple<int,int>(stoi(token),count));
+        if (token.at(0) !='x') bus_ids.push_back(tuple<int,int>(stoi(token),stoi(token)-count));
         data.at(1).erase(0, pos + 1);
         count++;
     }
-    for (int i = 0; i<bus_ids.size();i++)
-    {
-        cout << get<0>(bus_ids.at(i)) << " " << get<1>(bus_ids.at(i)) << endl;
-    }
-    if (data.at(1).at(0) !='x') bus_ids.push_back(tuple<int,int>(stoi(data.at(1).data()),count));
-//     cout << task_1(&bus_ids, target_time) << endl;
-     cout << task_2() << endl;
+    if (data.at(1).at(0) !='x') bus_ids.push_back(tuple<int,int>(stoi(data.at(1).data()),stoi(data.at(1).data())-count));
+    cout << "Task1: " << task_1(&bus_ids, target_time) << endl;
+     cout << "Task2: " << task_2(&bus_ids) << endl;
 }
